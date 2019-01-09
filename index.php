2dcf4bb6a1730b693da63963b59a97d8a7707292
nikhil-vinove <nikhil.gupta@mail.vinove.com>
2019-01-09 17:38:08 +0530

    intial commit

---
+++ index.php
@@ -0,0 +1,238 @@
+<?php
+$conn = mysqli_connect("localhost","root","","test");
+if( mysqli_connect_errno() ) {
+    echo mysqli_connect_error();
+} else {
+    echo('connected to db...<br /><br />');
+}
+require_once('vendor/php-excel-reader/excel_reader2.php');
+require_once('vendor/SpreadsheetReader.php');
+
+if (isset($_POST["import"]))
+{
+    
+    
+  $allowedFileType = ['application/vnd.ms-excel','text/xls','text/xlsx','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+  
+  if(in_array($_FILES["file"]["type"],$allowedFileType)){
+
+        $targetPath = 'uploads/'.$_FILES['file']['name'];
+        move_uploaded_file($_FILES['file']['tmp_name'], $targetPath);
+        
+        $Reader = new SpreadsheetReader($targetPath);
+        
+        $sheetCount = count($Reader->sheets());
+        for($i=0;$i<$sheetCount;$i++)
+        {
+            
+            $Reader->ChangeSheet($i);
+            
+            foreach ($Reader as $Row)
+            {
+          
+                $name = "";
+                if(isset($Row[0])) {
+                    $name = mysqli_real_escape_string($conn,$Row[0]);
+                }
+                print($name);
+                $description = "";
+                if(isset($Row[1])) {
+                    $description = mysqli_real_escape_string($conn,$Row[1]);
+                }
+                
+                if (!empty($name) || !empty($description)) {
+                    $query = "insert into tbl_info (name,description) values ('".$name."','".$description."')";
+                    print($query);
+                    $result = mysqli_query($conn, $query);
+                
+                    if (! empty($result)) {
+                        $type = "success";
+                        $message = "Excel Data Imported into the Database";
+                    } else {
+                        $type = "error";
+                        $message = "Problem in Importing Excel Data";
+                    }
+                }
+             }
+        
+         }
+  }
+  else
+  { 
+        $type = "error";
+        $message = "Invalid File Type. Upload Excel File.";
+  }
+}
+
+function callAPI($method, $url, $data){
+   $curl = curl_init();
+
+   switch ($method){
+      case "POST":
+         curl_setopt($curl, CURLOPT_POST, 1);
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, "prodId=2016070011220000103&orgId=1");
+         break;
+      case "PUT":
+         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);                              
+         break;
+      default:
+         if ($data)
+            $url = sprintf("%s?%s", $url, http_build_query($data));
+   }
+
+   // OPTIONS:
+   curl_setopt($curl, CURLOPT_URL, $url);
+   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/x-www-form-urlencoded',
+   ));
+   // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+   // curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+   // EXECUTE:
+   $result = curl_exec($curl);
+   if(!$result){die("Connection Failure");}
+   curl_close($curl);
+   return $result;
+}
+
+$data_array =  array(
+     'prodId'=>'2016070011220000103',
+     'orgId'=>'1'
+);
+
+
+$make_call = callAPI('POST', 'http://train.enam.gov.in/NamWebSrv/rest/assaying/getAssayingParamDtl', json_encode($data_array));
+$response = json_decode($make_call, true);
+echo '<pre>';
+print_r($response);die;
+?>
+
+<!DOCTYPE html>
+<html>    
+<head>
+<style>    
+body {
+	font-family: Arial;
+	width: 550px;
+}
+
+.outer-container {
+	background: #F0F0F0;
+	border: #e0dfdf 1px solid;
+	padding: 40px 20px;
+	border-radius: 2px;
+}
+
+.btn-submit {
+	background: #333;
+	border: #1d1d1d 1px solid;
+    border-radius: 2px;
+	color: #f0f0f0;
+	cursor: pointer;
+    padding: 5px 20px;
+    font-size:0.9em;
+}
+
+.tutorial-table {
+    margin-top: 40px;
+    font-size: 0.8em;
+	border-collapse: collapse;
+	width: 100%;
+}
+
+.tutorial-table th {
+    background: #f0f0f0;
+    border-bottom: 1px solid #dddddd;
+	padding: 8px;
+	text-align: left;
+}
+
+.tutorial-table td {
+    background: #FFF;
+	border-bottom: 1px solid #dddddd;
+	padding: 8px;
+	text-align: left;
+}
+
+#response {
+    padding: 10px;
+    margin-top: 10px;
+    border-radius: 2px;
+    display:none;
+}
+
+.success {
+    background: #c7efd9;
+    border: #bbe2cd 1px solid;
+}
+
+.error {
+    background: #fbcfcf;
+    border: #f3c6c7 1px solid;
+}
+
+div#response.display-block {
+    display: block;
+}
+</style>
+</head>
+
+<body>
+    <h2>Import Excel File into MySQL Database using PHP</h2>
+    
+    <div class="outer-container">
+        <form action="" method="post"
+            name="frmExcelImport" id="frmExcelImport" enctype="multipart/form-data">
+            <div>
+                <label>Choose Excel
+                    File</label> <input type="file" name="file"
+                    id="file" accept=".xls,.xlsx">
+                <button type="submit" id="submit" name="import"
+                    class="btn-submit">Import</button>
+        
+            </div>
+        
+        </form>
+        
+    </div>
+    <div id="response" class="<?php if(!empty($type)) { echo $type . " display-block"; } ?>"><?php if(!empty($message)) { echo $message; } ?></div>
+    
+         
+<?php
+    $sqlSelect = "SELECT * FROM tbl_info";
+    $result = mysqli_query($conn, $sqlSelect);
+
+if (mysqli_num_rows($result) > 0)
+{
+?>
+        
+    <table class='tutorial-table'>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+
+            </tr>
+        </thead>
+<?php
+    while ($row = mysqli_fetch_array($result)) {
+?>                  
+        <tbody>
+        <tr>
+            <td><?php  echo $row['name']; ?></td>
+            <td><?php  echo $row['description']; ?></td>
+        </tr>
+<?php
+    }
+?>
+        </tbody>
+    </table>
+<?php 
+} 
+?>
+
+</body>
+</html>
