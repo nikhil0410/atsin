6ceb099c7762a557e473ad5eebb2ea5964da7ade
nikhil-vinove <nikhil.gupta@mail.vinove.com>
2019-01-25 11:39:17 +0530

    update

--- index.php
+++ index.php
@@ -1,131 +1,8 @@
+
 <?php
-require_once('vendor/php-excel-reader/excel_reader2.php');
-require_once('vendor/SpreadsheetReader.php');
-
-if (isset($_POST["import"]))
-{
-    
-    
-  $allowedFileType = ['application/vnd.ms-excel','text/xls','text/xlsx','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
-  
-  if(in_array($_FILES["file"]["type"],$allowedFileType)){
-
-        $targetPath = 'uploads/'.$_FILES['file']['name'];
-        move_uploaded_file($_FILES['file']['tmp_name'], $targetPath);
-        
-        $Reader = new SpreadsheetReader($targetPath);
-        $saving_data = array(
-            'loginId'=>'HYDENTRY',
-            'password'=>'PASSWORD',
-            'orgId'=>'1',
-            'oprId'=>'49',
-            'lotId'=>'492017111332',
-            'sampleId'=>'1'
-        );
-        $saving_data = http_build_query($saving_data,'','&');
-        
-        $sheetCount = count($Reader->sheets());
-        $saving_data .= '&dtlList:[';
-        for($i=0;$i<$sheetCount;$i++)
-        {
-            // [{srNo:1,paramId:2016120011490000128,qtyUomId:2016040220980000001,prodId:2016040291220000007,type:N,nValue:5,maxVal:5.00,minVal:0.00}]
-
-            $Reader->ChangeSheet($i);
-            print(count($Reader));
-            foreach ($Reader as $Row)
-            {
-                // for Moisture
-          
-                $name = "";
-                $sr_no = 0;
-                if(isset($Row[8])) {
-                    if(strtoupper($Row[2]) == 'MUSTARDS'){
-                        // $name = mysqli_real_escape_string($conn,$Row[0]);
-                        if($Row['8'] != 'Moisture %'){
-                            $saving_data .= '{"srNo":'.$sr_no.',"paramId":"2016070011490000073","qtyUomId":"2016070011220000103","prodId":"2016070011220000103","type":"N","nValue":"'.$Row[8].'","maxVal":"7.50","minVal":"0.00"},';
-                        }elseif($Row['8'] != 'Oil As is %'){
-                            $saving_data .= '{"srNo":'.$sr_no.',"paramId":"2016070011490000093","qtyUomId":"2016070011220000103","prodId":"2016070011220000103","type":"N","nValue":"'.$Row[8].'","maxVal":"100.00","minVal":"36.00"},';
-                        }
-                    }
-                    elseif (strtolower($Row[2]) == 'soybeans') {
-                        if($Row['8'] != 'Moisture %'){
-                            $saving_data .= '{"srNo":'.$sr_no.',"paramId":"2016110011490000067","qtyUomId":"2016070011220000103","prodId":"2016080011220000021","type":"N","nValue":"'.$Row[8].'","maxVal":"12.00","minVal":"0.00"},';
-                        }elseif($Row['8'] != 'Oil As is %'){
-                            $saving_data .= '{"srNo":'.$sr_no.',"paramId":"2016110011490000073","qtyUomId":"2016070011220000103","prodId":"2016080011220000021","type":"N","nValue":"'.$Row[8].'","maxVal":"50.00","minVal":"15.00"},';
-                        }
-                    }
-                }
-                $sr_no +=1;
-             }
-        
-         }
-         $saving_data .= ']';
-
-                print($saving_data);
-                echo 'saving data \n';
-         $make_call = callAPI('POST', 'http://train.enam.gov.in/NamWebSrv/rest/assaying/getAssayingParamDtl', $saving_data);
-        // $response = json_decode($make_call, true);
-        echo 'Nikhil';
-        echo '<pre>';
-        // print_r($response);
-        die;
-  }
-  else
-  { 
-        $type = "error";
-        $message = "Invalid File Type. Upload Excel File.";
-  }
-}
-
-function callAPI($method, $url, $data){
-   $curl = curl_init();
-
-   switch ($method){
-      case "POST":
-         curl_setopt($curl, CURLOPT_POST, 1);
-         if ($data)
-            curl_setopt($curl, CURLOPT_POSTFIELDS, "prodId=2016070011220000103&orgId=1");
-         break;
-      case "PUT":
-         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-         if ($data)
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);                              
-         break;
-      default:
-         if ($data)
-            $url = sprintf("%s?%s", $url, http_build_query($data));
-   }
-
-   // OPTIONS:
-   curl_setopt($curl, CURLOPT_URL, $url);
-   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-      'Content-Type: application/x-www-form-urlencoded',
-   ));
-   // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-   // curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-
-   // EXECUTE:
-   $result = curl_exec($curl);
-   if(!$result){die("Connection Failure");}
-   curl_close($curl);
-   return $result;
-}
-
-$data_array =  array(
-     'prodId'=>'2016070011220000103',
-     'orgId'=>'1'
-);
-$sr=1;
-$data_array = http_build_query($data_array,'','&');
-$data_array .= '[{srNo'.$sr.'';
-
-
-// $make_call = callAPI('POST', 'http://train.enam.gov.in/NamWebSrv/rest/assaying/getAssayingParamDtl', $data_array);
-// $response = json_decode($make_call, true);
-// echo '<pre>';
-// print_r($response);die;
+// Turn off all error reporting
+error_reporting(0);
 ?>
-
 <!DOCTYPE html>
 <html>    
 <head>
@@ -134,6 +11,7 @@ body {
 	font-family: Arial;
 	width: 550px;
 }
+
 
 .outer-container {
 	background: #F0F0F0;
@@ -190,6 +68,11 @@ body {
     border: #f3c6c7 1px solid;
 }
 
+.tg  {border-collapse:collapse;border-spacing:0;border-color:#999;margin:0px auto;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:4px 19px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#999;color:#444;background-color:#F7FDFA;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:4px 19px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#999;color:#fff;background-color:#26ADE4;}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+
 div#response.display-block {
     display: block;
 }
@@ -197,17 +80,16 @@ div#response.display-block {
 </head>
 
 <body>
-    <h2>Import Excel File into ENAM.GOV.IN</h2>
+<h2>Import Excel File into ENAM.GOV.IN</h2>
     
     <div class="outer-container">
-        <form action="" method="post"
-            name="frmExcelImport" id="frmExcelImport" enctype="multipart/form-data">
+        <form action="" method="post" name="frmExcelImport" id="frmExcelImport" enctype="multipart/form-data">
             <div>
                 <label>Choose Excel
                     File</label> <input type="file" name="file"
                     id="file" accept=".xls,.xlsx">
-                <button type="submit" id="submit" name="import"
-                    class="btn-submit">Import</button>
+                <button type="submit" name="import"
+                    class="btn-submit">Upload to ENAM</button>
         
             </div>
         
@@ -219,27 +101,185 @@ div#response.display-block {
     
 
 
+<?php
+require_once('vendor/php-excel-reader/excel_reader2.php');
+require_once('vendor/SpreadsheetReader.php');
+
+if (isset($_POST["import"]))
+{?>
+<table class="tg">
+  <tr>
+    <th class="tg-0lax">srNo</th>
+    <th class="tg-0lax">paramId</th>
+    <th class="tg-0lax">prodId</th>
+    <th class="tg-0lax">Product</th>
+    <th class="tg-0lax">Value</th>
+    <th class="tg-0lax">qtyUomId</th>
+  </tr>
+
+<?php
+    
+    
+  $allowedFileType = ['application/vnd.ms-excel','text/xls','text/xlsx','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+  
+  if(in_array($_FILES["file"]["type"],$allowedFileType)){
+
+        $targetPath = 'uploads/'.$_FILES['file']['name'];
+        move_uploaded_file($_FILES['file']['tmp_name'], $targetPath);
+        
+        $Reader = new SpreadsheetReader($targetPath);
+        $saving_data = array(
+            'loginId'=>'RJ2024N00001',
+            'password'=>'PASSWORD',
+            'orgId'=>'1',
+            'oprId'=>'163',
+            'lotId'=>'492017111332',
+            'sampleId'=>'1'
+        );
+        $saving_data = http_build_query($saving_data,'','&');
+        
+        $sheetCount = count($Reader->sheets());
+        $saving_data .= '&dtlList=[';
+        $sr_no = 0;
+        for($i=0;$i<$sheetCount;$i++)
+        {
+            // [{srNo:1,paramId:2016120011490000128,qtyUomId:2016040220980000001,prodId:2016040291220000007,type:N,nValue:5,maxVal:5.00,minVal:0.00}]
+
+            
+            $Reader->ChangeSheet($i);
+            foreach ($Reader as $Row)
+            {
+                // for Moisture
+          
+                $name = "";
+                if(isset($Row[8])) {
+                    $product = strtoupper($Row[2]);
+
+                    if(strtoupper($Row[2]) == 'MUSTARDS'){
+                        // $name = mysqli_real_escape_string($conn,$Row[0]);
+                            $saving_data .= '{"srNo":'.$sr_no.',"paramId":"2016070011490000073","qtyUomId":"2016070011220000103","prodId":"2016070011220000103","type":"N","nValue":"'.$Row[8].'","maxVal":"7.50","minVal":"0.00"},';
+                        
+                    }elseif (strtolower($Row[2]) == 'soybeans') {
+                            $saving_data .= '{"srNo":'.$sr_no.',"paramId":"2016110011490000067","qtyUomId":"2016070011220000103","prodId":"2016080011220000021","type":"N","nValue":"'.$Row[8].'","maxVal":"12.00","minVal":"0.00"},';
+                    }
+
+                    ?>
+                    <tr>
+                        <td class="tg-0lax"><? echo $sr_no; ?></td>
+                        <td class="tg-0lax"><? echo $product; ?></td>
+                        <td class="tg-0lax"><? echo 'Moisture'; ?></td>
+                        <td class="tg-0lax"><? echo $Row[8]; ?></td>
+                        <td class="tg-0lax"><? echo $sr_no; ?></td>
+                        <td class="tg-0lax"><? echo $sr_no; ?></td>
+                    </tr>
+                    <?php 
+                    $sr_no +=1;
+                }
+                if(isset($Row[8])) {
+                    $product = strtoupper($Row[2]);
+
+                    if(strtoupper($Row[2]) == 'MUSTARDS'){
+                            $saving_data .= '{"srNo":'.$sr_no.',"paramId":"2016070011490000093","qtyUomId":"2016070011220000103","prodId":"2016070011220000103","type":"N","nValue":"'.$Row[8].'","maxVal":"100.00","minVal":"36.00"},';
+                        
+                    }elseif (strtolower($Row[2]) == 'soybeans') {
+                            $saving_data .= '{"srNo":'.$sr_no.',"paramId":"2016110011490000073","qtyUomId":"2016070011220000103","prodId":"2016080011220000021","type":"N","nValue":"'.$Row[8].'","maxVal":"50.00","minVal":"15.00"},';
+                    }
+
+                    ?>
+                    <tr>
+                        <td class="tg-0lax"><? echo $sr_no; ?></td>
+                        <td class="tg-0lax"><? echo $product; ?></td>
+                        <td class="tg-0lax"><? echo 'Oil As is %' ?></td>
+                        <td class="tg-0lax"><? echo $Row[9]; ?></td>
+                        <td class="tg-0lax"><? echo $sr_no; ?></td>
+                        <td class="tg-0lax"><? echo $sr_no; ?></td>
+                    </tr>
+                    <?php 
+                    $sr_no +=1;
+                }
+             }
+         }
+         $saving_data .= ']';
+         print($saving_data);
+
+        $url = 'http://train.enam.gov.in/NamWebSrv/rest/assaying/submitAssayingDtl';
+        $data = array('key1' => 'value1', 'key2' => 'value2');
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => $saving_data
+                )
+            );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        // $result = json_encode($result);
+        $result = json_decode($result, true);
+        if($result['statusMsg'] == 'S'){
+            print('Success');
+        }else{
+            print('Failure');
+        }
+  }
+  else
+  { 
+        $type = "error";
+        $message = "Invalid File Type. Upload Excel File.";
+  }
+}
+
+
+$data_array =  array(
+     'prodId'=>'2016070011220000103',
+     'orgId'=>'1'
+);
+$sr=1;
+$data_array = http_build_query($data_array,'','&');
+$data_array .= '[{srNo'.$sr.'';
+?>
+</table>
+
+    
+
+
 </body>
 <script type="text/javascript">
+// document.getElementById('frmExcelImport').addEventListener('submit', test, false                     )
 
+function test(e){
+    e.preventDefault();
+    var url = 'http://train.enam.gov.in/NamWebSrv/rest/assaying/getSampleAssayingLots';
+    var data = {
+            loginId:'RJ2024N00001',
+            password:'PASSWORD',
+            orgId:'1',
+            oprId:'163',
+            fromDate:'2018-11-27',
+            toDate:'2018-11-28'
+        };
+    const toUrlEncoded = obj => Object.keys(obj).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k])).join('&');
 
-function test(){
-var url = 'http://train.enam.gov.in/NamWebSrv/rest/assaying/getAssayingParamDtl';
-var data = {
-        prodId: '2016070011220000103',
-        orgId: '1'
-    };
-const toUrlEncoded = obj => Object.keys(obj).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(obj[k])).join('&');
+    fetch(url, {
+      method: 'POST', // or 'PUT'
+      body: toUrlEncoded(data), // data can be `string` or {object}!
+      headers:{;
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(res => res.json())
+    .then(function(response){
+        console.log('Success:', (response))
+            var resp = (response);
+            console.log(resp['statusMsg']);
+            document.getElementById('frmExcelImport').submit();
+    } 
+        )
+    .catch(error => console.error('Error:', error));
+}
 
-fetch(url, {
-  method: 'POST', // or 'PUT'
-  body: toUrlEncoded(data), // data can be `string` or {object}!
-  headers:{
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
-}).then(res => res.json())
-.then(response => console.log('Success:', JSON.stringify(response)))
-.catch(error => console.error('Error:', error));
+function getData() {
+    fetch('')
 }
 
 
